1f27306020a43084435795d1112596300b0a7412
bencode <bencode@163.com>
2019-03-11 10:06:40 +0800

    init html template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'normalize.css';
+import './App.scss';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="sidebar">
+          <div className="header">
+            <button className="button adder">
+              <i className="iconfont icon-add"></i>
+              新建笔记
+            </button>
+          </div>
+          <div className="body">
+            <div className="notebooks">
+              <div className="header has-icon">
+                <i className="iconfont icon-books"></i>
+                笔记本
+              </div>
+              <div className="body">
+                <ul className="notebooks-list">
+                  <li className="notebook-item">
+                    <div className="title has-icon">
+                      <i className="iconfont icon-book"></i>
+                      默认笔记本
+                    </div>
+                    <button className="button trash"><i className="iconfont icon-trash"></i></button>
+                  </li>
+                  <li className="notebook-item">
+                    <div className="title has-icon">
+                      <i className="iconfont icon-book"></i>
+                      读书笔记
+                    </div>
+                    <button className="button trash"><i className="iconfont icon-trash"></i></button>
+                  </li>
+                  <li className="notebook-item">
+                    <div className="title has-icon">
+                      <i className="iconfont icon-book"></i>
+                      吃喝玩乐
+                    </div>
+                    <button className="button trash"><i className="iconfont icon-trash"></i></button>
+                  </li>
+                  <li className="notebook-item">
+                    <div className="title has-icon">
+                      <i className="iconfont icon-book"></i>
+                      我的2019
+                    </div>
+                    <button className="button trash"><i className="iconfont icon-trash"></i></button>
+                  </li>
+              </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="notes-panel">
+          <div className="header">读书笔记</div>
+          <div className="body">
+            <ul className="notes-list">
+              <li>
+                <div className="note-brief">
+                  <div className="header">新建笔记</div>
+                  <div className="body">
+                    笔记简要内容
+                  </div>
+                  <div className="footer">
+                    <div className="datetime">2019-3-5</div>
+                    <button className="trash button">
+                      <i className="iconfont icon-trash"></i>
+                    </button>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="note-panel">
+          <div className="header">
+            <div className="category has-icon">
+              <i className="iconfont icon-notebook"></i>
+              我的2018
+            </div>
+            <div className="title">
+              <input type="text" value="标题" />
+            </div>
+          </div>
+          <div className="body">
+            <div className="editor">
+              <textarea></textarea>
+            </div>
+            <div className="preview">
+              <div>内容</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
